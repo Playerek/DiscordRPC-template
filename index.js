1634adde70265ce498601429d7b9769864a7757d
Playerek <animacje-mc@wp.pl>
2021-04-19 15:37:30 +0200

    Pliki

---
+++ index.js
@@ -0,0 +1,22 @@
+const rpc = require("discord-rpc");
+const client = new rpc.Client({ transport: 'ipc' });
+
+let IDaplikacji = "ID_TWOJEJ_APLIKACJI";
+
+client.on('ready', () => {
+    client.request('SET_ACTIVITY', {
+        pid: process.pid,
+        activity : {
+            details : "Opis",
+            assets : {
+            large_image : "nazwa-obrazka", 
+            large_text : "Tekst po najechaniu obrazka"
+            },
+            buttons : [{label : "Przycisk 1", url : "https://google.com/"},{label : "Przycisk 2", url : "https://google.com/"}]
+        }
+    }).then(x => {
+        console.log(`Status jest wyświetlany przy pomocy aplikacji ${x.name}!`)
+    })
+})
+
+client.login({ clientId : IDaplikacji }).catch(console.error);
